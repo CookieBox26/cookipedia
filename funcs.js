c548d6add31ce98b617db0154a92ba1f516a1b2b
CookieBox26 <yurii_wednesday@yahoo.co.jp>
2023-10-01 16:22:37 +0900

    Add files via upload

--- funcs.js
+++ funcs.js
@@ -7,7 +7,7 @@ function createSidebar() {
     var gitHub = '<a href="https://github.com/CookieBox26/cookipedia/issues">Issues</a>';
     var message = '何かありましたら ' + gitHub + ' までご連絡ください';
 
-    var index = '<h5>このページの小見出し一覧</h5>';
+    var index = '<h5>ページ内の小見出し一覧</h5>';
     var allHeaders = document.querySelectorAll('h2, h3');
     for (var i=0; i<allHeaders.length; ++i) {
         index += '<p class="';
@@ -22,7 +22,7 @@ function createSidebar() {
 
     var content = '<p class="contact">' + message + '</p><br/>';
     var back = '<a href="../index.html">メインページに戻る</a>';
-    content += '<p><a href="#">このページの一番上に戻る</a></p><p>' + back + '</p><br/>';
+    content += '<p><a href="#">ページの一番上に戻る</a></p><p>' + back + '</p><br/>';
     content += '<div id="headers">' + index + '</div>';
     document.getElementById('sidebar').innerHTML += content;
     document.getElementById('smartphone-header').innerHTML += back;
@@ -40,8 +40,8 @@ function syntaxHighlight() {
             re.push([/(#.*?)(<br\/>)/g, '<span__comment__>$1</span>$2']);
             re.push([/("""|''')(.*?)("""|''')/g, '<span__comment__>$1$2$3</span>']);
             // 数値
-            re.push([/([+-]?[0-9]+[\.]?[0-9]?(?:[eE][+-]?[0-9]+)?)/g,
-                     '<span__numeric__>$1</span>']);
+            re.push([/(<br\/>|\s|\[|\(|\{|=)([+-]?[0-9]+[\.]?[0-9]?(?:[eE][+-]?[0-9]+)?)/g,
+                     '$1<span__numeric__>$2</span>']);
             re.push([/(True|False|None)/g, '<span__numeric__>$1</span>']);
             // キーワード
             re.push([/(<br\/>|\s)(and|as|assert|async|await|break|class)(<br\/>|\s)/g,
@@ -53,7 +53,7 @@ function syntaxHighlight() {
             re.push([/(<br\/>|\s)(or|pass|raise|return|try|while|with|yield)(<br\/>|\s)/g,
                      '$1<span__keyword__>$2</span>$3']);
             // 組み込み関数
-            re.push([/(<br\/>|\s)(abs|all|any|dict|enumerate|len|print|type)(\()/g,
+            re.push([/(<br\/>|\s)(abs|all|any|dict|enumerate|len|print|type|int|float)(\()/g,
                      '$1<span__builtin__>$2</span>$3']);
         }
         if (pres[i].classList.contains('html')) {
@@ -62,6 +62,18 @@ function syntaxHighlight() {
             re.push([/([^'])(')([^']*)(')([^'])/g, '$1<span__literal__>$2$3$4</span>$5']);
             re.push([/(&lt;|\/)(html|head|body|style|div|br)(&gt;|\s|\/)/g,
                      '$1<span__keyword__>$2</span>$3']);
+        }
+        if (pres[i].classList.contains('sh')) {
+            re.push([/([^"])(")([^"]*)(")([^"])/g, '$1<span__literal__>$2$3$4</span>$5']);
+            re.push([/([^'])(')([^']*)(')([^'])/g, '$1<span__literal__>$2$3$4</span>$5']);
+            re.push([/(<br\/>|\s)(#.*?)(<br\/>)/g, '$1<span__comment__>$2</span>$3']);
+            re.push([/(<br\/>|\s)(if|then|else|elif|fi|case|esac|for|select)(<br\/>|\s)/g,
+                     '$1<span__keyword__>$2</span>$3']);
+            re.push([/(<br\/>|\s)(while|until|do|done|in|function|time)(<br\/>|\s)/g,
+                     '$1<span__keyword__>$2</span>$3']);
+            re.push([/(\$\{)([^\}]*)(\})/g, '<span__builtin__>$1$2$3</span>']);
+            re.push([/(\$)([^\s\{\(]*)(\s|<br\/>)/g, '<span__builtin__>$1$2$3</span>']);
+            re.push([/(\$)(\()/g, '<span__builtin__>$1</span>$2']);
         }
         if (re.length == 0) {
             continue;
