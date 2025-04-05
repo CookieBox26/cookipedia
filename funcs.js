c20fca5fe676afd5beb5385c1126e2ff8fc64b01
CookieBox26 <yurii_wednesday@yahoo.co.jp>
2025-04-05 17:19:10 +0900

    Add files via upload

--- funcs.js
+++ funcs.js
@@ -38,87 +38,30 @@ function createSidebar(mainPage=false) {
     }
 }
 
-function syntaxHighlight() {
-    // [2024/10/27] Prism をつかうようにしたのでこの関数は呼ばないようにした
-    let pres = document.getElementsByTagName('pre');
-    for (let i=0; i < pres.length; ++i) {
-        let re = [];
-        if (pres[i].classList.contains('python')) {
-            // マジックコマンド (が2行続いてもハイライトするために2回置換する苦肉の策)
-            re.push([/(<br\/>)(%[_a-z0-9\s]*)(<br\/>)/g, '$1<span__builtin__>$2</span>$3']);
-            re.push([/(<br\/>)(%[_a-z0-9\s]*)(<br\/>)/g, '$1<span__builtin__>$2</span>$3']);
-            // 文字列リテラル
-            re.push([/([^"])(")([^"]*)(")([^"])/g, '$1<span__literal__>$2$3$4</span>$5']);
-            re.push([/([^'])(')([^']*)(')([^'])/g, '$1<span__literal__>$2$3$4</span>$5']);
-            // コメント
-            re.push([/(#.*?)(<br\/>)/g, '<span__comment__>$1</span>$2']);
-            re.push([/("""|''')(.*?)("""|''')/g, '<span__comment__>$1$2$3</span>']);
-            // 数値
-            re.push([/(<br\/>|\s|\[|\(|\{|=)([+-]?[0-9]+[\.]?[0-9]?(?:[eE][+-]?[0-9]+)?)/g, '$1<span__numeric__>$2</span>']);
-            re.push([/(True|False|None)/g, '<span__numeric__>$1</span>']);
-            // キーワード
-            re.push([/(<br\/>|\s)(and|as|assert|async|await|break|class)(<br\/>|\s)/g, '$1<span__keyword__>$2</span>$3']);
-            re.push([/(<br\/>|\s)(continue|def|del|elif|else|except|finally|for)(<br\/>|\s)/g, '$1<span__keyword__>$2</span>$3']);
-            re.push([/(<br\/>|\s)(from|global|if|import|in|is|lambda|nonlocal|not)(<br\/>|\s)/g, '$1<span__keyword__>$2</span>$3']);
-            re.push([/(<br\/>|\s)(or|pass|raise|return|try|while|with|yield)(<br\/>|\s)/g, '$1<span__keyword__>$2</span>$3']);
-            // 組み込み関数
-            re.push([/(<br\/>|\s)(abs|all|any|dict|enumerate|len|print|type|int|float)(\()/g, '$1<span__builtin__>$2</span>$3']);
-        }
-        if (pres[i].classList.contains('html')) {
-            re.push([/(&lt;!--)(.*?)(--&gt;)/g, '<span__comment__>$1$2$3</span>']);
-            re.push([/([^"])(")([^"]*)(")([^"])/g, '$1<span__literal__>$2$3$4</span>$5']);
-            re.push([/([^'])(')([^']*)(')([^'])/g, '$1<span__literal__>$2$3$4</span>$5']);
-            re.push([/(&lt;|\/)(html|head|body|style|div|br)(&gt;|\s|\/)/g, '$1<span__keyword__>$2</span>$3']);
-        }
-        if (pres[i].classList.contains('sh')) {
-            re.push([/([^"])(")([^"]*)(")([^"])/g, '$1<span__literal__>$2$3$4</span>$5']);
-            re.push([/([^'])(')([^']*)(')([^'])/g, '$1<span__literal__>$2$3$4</span>$5']);
-            re.push([/(<br\/>|\s)(#.*?)(<br\/>)/g, '$1<span__comment__>$2</span>$3']);
-            re.push([/(<br\/>|\s)(if|then|else|elif|fi|case|esac|for|select)(<br\/>|\s)/g, '$1<span__keyword__>$2</span>$3']);
-            re.push([/(<br\/>|\s)(while|until|do|done|in|function|time)(<br\/>|\s)/g, '$1<span__keyword__>$2</span>$3']);
-            re.push([/(\$\{)([^\}]*)(\})/g, '<span__builtin__>$1$2$3</span>']);
-            re.push([/(\$)([^\s\{\(]*)(\s|<br\/>)/g, '<span__builtin__>$1$2$3</span>']);
-            re.push([/(\$)(\()/g, '<span__builtin__>$1</span>$2']);
-        }
-        if (re.length == 0) {
-            continue;
-        }
-        let code = pres[i].innerHTML;
-        // 文頭・改行・文末を扱いやすくするためにすべて <br/> に置換する．
-        code = '<br/>' + code.replace(/\n/g, '<br/>') + '<br/>';
-        for (let i=0; i < re.length; ++i) {
-            code = code.replace(re[i][0], re[i][1]);
-        }
-        code = code.replace(/span__(.*?)__/g, 'span class="$1"');
-        code = code.replace(/^<br\/>/, '');
-        code = code.replace(/<br\/>$/, '');
-        pres[i].innerHTML = code;
-    }
-}
-
-function init(mainPage=false) {
-    if (!mainPage) prepareTitle();
-    createSidebar(mainPage);
-    // syntaxHighlight();
-
-    const links = document.querySelectorAll('a');
-    links.forEach((elem) => {
-        const href = elem.getAttribute('href');
-        if (href && !href.startsWith('#') && !href.startsWith('..')) {
-            elem.setAttribute('target', '_blank');
-            elem.setAttribute('rel', 'noopener noreferrer');
-        }
-    });
-}
 function setButton(id, handle) {
     let button = document.getElementById(id);
     button.addEventListener("click", handle);
     button.addEventListener("touchstart", handle);
 }
+
 function setButtonOpenClose(id0, id1) {
     let target = document.getElementById(id1);
     target.style.display = "none";
     setButton(id0, () => {
         target.style.display = (target.style.display == "none") ? "block" : "none";
+    });
+}
+
+function init(mainPage=false) {
+    if (!mainPage) prepareTitle();
+    createSidebar(mainPage);
+
+    const links = document.querySelectorAll('a');
+    links.forEach((elem) => {
+        const href = elem.getAttribute('href');
+        if (href && href.startsWith('http')) {
+            elem.setAttribute('target', '_blank');
+            elem.setAttribute('rel', 'noopener noreferrer');
+        }
     });
 }
