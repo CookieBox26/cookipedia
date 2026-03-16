60b138fc615cbe7d4c8074e6d9cb8a505ccf0b5d
CookieBox26 <yurii_wednesday@yahoo.co.jp>
2026-03-16 14:31:38 +0900

    Adapt to changes in site-utils

--- build.py
+++ build.py
@@ -5,26 +5,22 @@
 # ]
 # [tool.uv.sources.cookies_site_utils]
 # git = "https://github.com/CookieBox26/cookies-site-utils"
-# rev = "1e2db74afb9eb3402646dab686cd1d3b5c9a3801"
+# rev = "8d42eaa355c5f54f0a440a65a9ce1e0f8f058276"
 # ///
-from cookies_site_utils.builder import build_index, IndexPage, validate
+from cookies_site_utils.resources import sync_resource
+from cookies_site_utils.builder import build_index, IndexPage, find_disallowed
 from pathlib import Path
-import logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(levelname)s] %(message)s',
-)
 
 
 if __name__ == '__main__':
     work_root = Path(__file__).resolve().parent
     site_root = work_root / 'docs'
-    style_css = site_root / 'css/style.css'
-    funcs_js = site_root / 'funcs.js'
-    last_counts_path = work_root / '.last_counts.toml'
+    sync_resource(site_root / 'css/style.css')
+    sync_resource(site_root / 'funcs.js')
 
     with build_index(
-        site_root, style_css, funcs_js, last_counts_path,
+        site_root,
+        last_counts_path=(work_root / '.last_counts.toml'),
         force_keep_timestamp=False,
     ):
         _ = IndexPage(
@@ -32,8 +28,12 @@ if __name__ == '__main__':
             work_root / 'templates',
             'Cookipedia α-version',
         )
-        validate(
-            site_root,
-            ['funcs.js', 'index.html'],
-            ['css', 'articles', 'categories', 'utils'],
-        )
+
+    find_disallowed(site_root, allowlist=[
+        'funcs.js',
+        'css/*.css',
+        'index.html',
+        'categories/*.html',
+        'articles/*.html',
+        'utils/*.html',
+    ])
