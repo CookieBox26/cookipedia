361a3358ba995408733745fca7e5099319f52109
CookieBox26 <yurii_wednesday@yahoo.co.jp>
2026-07-19 18:04:07 +0900

    Update sample plot

--- docs/images/mpl001.py
+++ docs/images/mpl001.py
@@ -4,25 +4,26 @@ from pathlib import Path
 
 
 def main():
-    out = Path(__file__).stem + '.png'
+    out = Path(__file__).resolve().with_suffix('.png')
 
     x = [0, 1, 2, 3, 4]
     y = [3.5, 2.6, 1.8, 1.5, 1.4]
 
-    with plt.rc_context({
-        'figure.figsize': (4, 3),
-        'font.family': 'Yu Gothic UI',
-        'font.size': 13,
-    }):
+    context = {}
+    context['font.family'] = 'Segoe UI'
+    # for key in ['font.weight', 'axes.labelweight']:
+    #     context[key] = 'semibold'
+    context['font.size'] = 13
+    with plt.rc_context(context):
         desc = {}
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(3.5, 2))
 
         desc['Training loss'] = ax.plot(x, y)[0]
 
         ax.set_xlabel('Epoch')
         ax.set_ylabel('Training loss')
         ax.xaxis.set_major_locator(MultipleLocator(1))
-        ax.yaxis.set_major_locator(MultipleLocator(0.25))
+        ax.yaxis.set_major_locator(MultipleLocator(0.5))
         plt.grid(axis='both', linestyle='dotted')
         plt.legend(
             desc.values(), desc.keys(),
